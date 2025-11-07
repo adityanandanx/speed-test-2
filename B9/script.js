e34bed93d08416d2a5b6fb305146c9705cc73a96
Aditya Nandan <nandanaditya985@gmail.com>
2025-11-07 15:17:32 +0530

    b9

--- B9/script.js
+++ B9/script.js
@@ -1,15 +1,68 @@
 document.addEventListener("DOMContentLoaded", (e) => {
+  /**@type {HTMLInputElement} */
   const inp = document.querySelector("#pass");
-  const mask = document.querySelector("#mask");
+  const show = document.querySelector("#show");
+
   let pass = "";
-  let random = "";
-  const pool = "!@#$%^&*()_+";
+  let mask = "";
+  let hidden = true;
 
-  inp.addEventListener("input", (e) => {
-    random += pool.at(Math.floor(Math.random() * (pool.length - 1)));
-    pass = e.target.value;
+  show.addEventListener("click", (e) => {
+    hidden = !hidden;
 
-    random = random.slice(0, pass.length);
-    mask.value = random;
+    if (hidden) {
+      show.textContent = "Show";
+      inp.value = mask;
+    } else {
+      show.textContent = "Hide";
+      inp.value = pass;
+    }
+  });
+
+  const getRandom = () => {
+    const pool = "!@#$%^&*()_+";
+    return pool.charAt(Math.floor(Math.random() * pool.length));
+  };
+
+  inp.addEventListener("keydown", (e) => {
+    if (["ArrowLeft", "Home", "End", "ArrowRight", "Tab"].includes(e.key)) {
+      return;
+    }
+
+    e.preventDefault();
+
+    let start = inp.selectionStart;
+    let end = inp.selectionEnd;
+
+    if (e.key === "Backspace") {
+      if (start === end && start !== 0) {
+        pass = pass.slice(0, start - 1) + pass.slice(start);
+        mask = mask.slice(0, start - 1) + mask.slice(start);
+        start--;
+      } else if (start !== end) {
+        pass = pass.slice(0, start) + pass.slice(end);
+        mask = mask.slice(0, start) + mask.slice(end);
+      }
+    } else if (e.key === "Delete") {
+      if (start === end && start !== 0) {
+        pass = pass.slice(0, start) + pass.slice(start + 1);
+        mask = mask.slice(0, start) + mask.slice(start + 1);
+      } else if (start !== end) {
+        pass = pass.slice(0, start) + pass.slice(end);
+        mask = mask.slice(0, start) + mask.slice(end);
+      }
+    } else if (e.key.length === 1 && (!e.ctrlKey || !e.metaKey)) {
+      pass = pass.slice(0, start) + e.key + pass.slice(end);
+      mask = mask.slice(0, start) + getRandom() + mask.slice(end);
+      start++;
+    }
+
+    if (hidden) {
+      inp.value = mask;
+    } else {
+      inp.value = pass;
+    }
+    console.log({ pass, mask });
+    inp.setSelectionRange(start, start);
   });
 });
